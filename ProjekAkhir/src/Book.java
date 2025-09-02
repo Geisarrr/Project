76e67a43d3ad95c379084d8de5c6e45ff363f56e
Geisarrr <rampangeisar@gmail.com>
2025-09-02 12:26:20 +0700

    Projek Akhir Pemlan

---
+++ ProjekAkhir/src/Book.java
@@ -0,0 +1,12 @@
+// Book.java
+public class Book {
+    String judul, penulis;
+    int tahun, stok;
+
+    Book(String judul, String penulis, int tahun, int stok) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahun = tahun;
+        this.stok = stok;
+    }
+}
